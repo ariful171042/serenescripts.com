59ac5933b976ea21f24948db073acb27d0ec9f22
ariful171042 <arif171042@gmail.com>
2024-02-26 22:35:05 +0600

    single page

---
+++ app/[id]/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BlogDetailsPage = ({ params }: { params: { id: string } }) => {
+  return <div className="sp wrapper">
+    <div className="">
+      <div className="">
+        //Image
+      </div>
+      <div className="">
+        <h2>Title</h2>
+      </div>
+    </div>
+  </div>;
+};
+
+export default BlogDetailsPage;
