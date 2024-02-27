38514cc80c9c55224793f52218deecb37a6579a4
ariful171042 <arif171042@gmail.com>
2024-02-27 23:25:03 +0600

    single page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 
 const BlogDetailsPage = ({ params }: { params: { id: string } }) => {
-  return <div className="sp wrapper">
-    <div className="">
+  return (
+    <div className="sp wrapper">
       <div className="">
-        //Image
-      </div>
-      <div className="">
-        <h2>Title</h2>
+        <div className="">//Image</div>
+        <div className="">
+          <h2>Title</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+            numquam atque? Voluptatum magni odit voluptas itaque quis. Quis, ea.
+            Recusandae amet voluptate nisi reprehenderit ut qui possimus totam.
+            Vero ex necessitatibus libero inventore commodi cum unde eum
+            repellat est pariatur et odio at ipsum dolorem, animi voluptate
+            cupiditate tempore qui?
+          </p>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default BlogDetailsPage;
